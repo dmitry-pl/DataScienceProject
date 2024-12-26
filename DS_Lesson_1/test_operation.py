de1b755e70d16120b7f8c48a209eeb73ade59c2f
dmitry-pl <dmitry.pleshko.ggle@gmail.com>
2024-12-26 20:03:29 +0300

    Test

---
+++ DS_Lesson_1/test_operation.py
@@ -0,0 +1,11 @@
+# math_operations.py импортируется из файла
+
+# main.py
+import math_operations as mp
+#from math_operations import add, subtract
+
+result = mp.add(20, 3)
+print("Сумма:", result)
+
+result = mp.subtract(5, 3)
+print("Разница:", result)
